2be1c9c0e517c58c3550a8fc5d0a996db42ed9d3
Marcel du Preez <marceldupr@gmail.com>
2014-03-09 08:41:05 +0000

    added better handler resolution

--- app/query-service.js
+++ app/query-service.js
@@ -1,10 +1,6 @@
 var service = require('../lib/service'),
-    message = require('../lib/message');
-
-var handlers = [
-  require('./handlers/query/city-query'),
-  require('./handlers/query/city-query-response')
-];
+    message = require('../lib/message'),
+    handlers = require('./handlers');
 
 service('Query Service', handlers, function(bus){
   bus.send(message.createTransient('cityQuery', 'UK'));
